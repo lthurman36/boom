6a42a4fc48c5e7bf82442491893f446148e1bc95
ngmosquera <75039891+ngmosquera@users.noreply.github.com>
2021-04-13 17:53:17 -0600

    Added validateFrom function
    Code to validate our contact form (located on the Contact Us page)

--- scripts.js
+++ scripts.js
@@ -13,3 +13,18 @@ function stickyFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+function validateForm() {
+    fieldsValid = false;
+    fieldsValidated = 0
+        for (i=0; i < document.forms["contactForm"].getElementsByTagName("input").length;i++){
+            if(document.forms["contactForm"][i].type === "text" || document.forms["contactForm"][i].type === "date") {
+            if (document.forms["contactForm"][i].element.value === "") 
+                $('.alert').removeClass('d-none').addClass('show');
+                document.forms["contactForm"][i].element.classList.add("required-input");
+            } else {
+                document.forms["contactForm"][i].element.classList.remove("required-input");
+                fieldsValidated += 1;
+            }
+        }
+   
